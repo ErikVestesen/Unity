da1a68dc101678ddb29cd29141294413f62c2ca0
Erik Vestesen <Craster013014@live.dk>
2016-12-16 02:38:03 +0100

    Dialogue System started, needs manager script

---
+++ SimpleRpg/Assets/Scripts/Interactable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour {
+    public NavMeshAgent playerAgent;
+    private bool hasInteracted;
+    // move to object and interact with object.
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent) {
+        hasInteracted = false;
+        this.playerAgent = playerAgent;
+        playerAgent.stoppingDistance = 2f;
+        playerAgent.destination = this.transform.position;
+    }
+
+    void Update()
+    {
+        if(playerAgent != null && !playerAgent.pathPending) {
+            if(playerAgent.remainingDistance <= playerAgent.stoppingDistance && !hasInteracted)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with base class.");
+    }
+
+}
