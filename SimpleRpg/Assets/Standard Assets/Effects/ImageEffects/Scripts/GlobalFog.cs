b71f219bfa7f591dfd93907cd1847e65f254931f
Erik Vestesen <Craster013014@live.dk>
2016-12-18 00:19:47 +0100

    Weapon equipping system --Started

--- SimpleRpg/Assets/Standard Assets/Effects/ImageEffects/Scripts/GlobalFog.cs
+++ SimpleRpg/Assets/Standard Assets/Effects/ImageEffects/Scripts/GlobalFog.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 namespace UnityStandardAssets.ImageEffects
 {
     [ExecuteInEditMode]
-    [RequireComponent (typeof(Camera))]
-    [AddComponentMenu ("Image Effects/Rendering/Global Fog")]
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("Image Effects/Rendering/Global Fog")]
     class GlobalFog : PostEffectsBase
-	{
-		[Tooltip("Apply distance-based fog?")]
-        public bool  distanceFog = true;
-		[Tooltip("Exclude far plane pixels from distance-based fog? (Skybox or clear color)")]
-		public bool  excludeFarPixels = true;
-		[Tooltip("Distance fog is based on radial distance from camera when checked")]
-		public bool  useRadialDistance = false;
-		[Tooltip("Apply height-based fog?")]
-		public bool  heightFog = true;
-		[Tooltip("Fog top Y coordinate")]
+    {
+        [Tooltip("Apply distance-based fog?")]
+        public bool distanceFog = true;
+        [Tooltip("Exclude far plane pixels from distance-based fog? (Skybox or clear color)")]
+        public bool excludeFarPixels = true;
+        [Tooltip("Distance fog is based on radial distance from camera when checked")]
+        public bool useRadialDistance = false;
+        [Tooltip("Apply height-based fog?")]
+        public bool heightFog = true;
+        [Tooltip("Fog top Y coordinate")]
         public float height = 1.0f;
-        [Range(0.001f,10.0f)]
+        [Range(0.001f, 10.0f)]
         public float heightDensity = 2.0f;
-		[Tooltip("Push fog away from the camera by this amount")]
+        [Tooltip("Push fog away from the camera by this amount")]
         public float startDistance = 0.0f;
 
         public Shader fogShader = null;
         private Material fogMaterial = null;
 
 
-        public override bool CheckResources ()
-		{
-            CheckSupport (true);
+        public override bool CheckResources()
+        {
+            CheckSupport(true);
 
-            fogMaterial = CheckShaderAndCreateMaterial (fogShader, fogMaterial);
+            fogMaterial = CheckShaderAndCreateMaterial(fogShader, fogMaterial);
 
             if (!isSupported)
-                ReportAutoDisable ();
+                ReportAutoDisable();
             return isSupported;
         }
 
@@ -50,24 +50,46 @@ namespace UnityStandardAssets.ImageEffects
             Camera cam = GetComponent<Camera>();
             Transform camtr = cam.transform;
 
-            Vector3[] frustumCorners = new Vector3[4];
-            cam.CalculateFrustumCorners(new Rect(0, 0, 1, 1), cam.farClipPlane, cam.stereoActiveEye, frustumCorners);
-            var bottomLeft = camtr.TransformVector(frustumCorners[0]);
-            var topLeft = camtr.TransformVector(frustumCorners[1]);
-            var topRight = camtr.TransformVector(frustumCorners[2]);
-            var bottomRight = camtr.TransformVector(frustumCorners[3]);
+            float camNear = cam.nearClipPlane;
+            float camFar = cam.farClipPlane;
+            float camFov = cam.fieldOfView;
+            float camAspect = cam.aspect;
 
-            Matrix4x4 frustumCornersArray = Matrix4x4.identity;
-            frustumCornersArray.SetRow(0, bottomLeft);
-            frustumCornersArray.SetRow(1, bottomRight);
-            frustumCornersArray.SetRow(2, topLeft);
-            frustumCornersArray.SetRow(3, topRight);
+            Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+            float fovWHalf = camFov * 0.5f;
+
+            Vector3 toRight = camtr.right * camNear * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+            Vector3 toTop = camtr.up * camNear * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+            Vector3 topLeft = (camtr.forward * camNear - toRight + toTop);
+            float camScale = topLeft.magnitude * camFar / camNear;
+
+            topLeft.Normalize();
+            topLeft *= camScale;
+
+            Vector3 topRight = (camtr.forward * camNear + toRight + toTop);
+            topRight.Normalize();
+            topRight *= camScale;
+
+            Vector3 bottomRight = (camtr.forward * camNear + toRight - toTop);
+            bottomRight.Normalize();
+            bottomRight *= camScale;
+
+            Vector3 bottomLeft = (camtr.forward * camNear - toRight - toTop);
+            bottomLeft.Normalize();
+            bottomLeft *= camScale;
+
+            frustumCorners.SetRow(0, topLeft);
+            frustumCorners.SetRow(1, topRight);
+            frustumCorners.SetRow(2, bottomRight);
+            frustumCorners.SetRow(3, bottomLeft);
 
             var camPos = camtr.position;
             float FdotC = camPos.y - height;
             float paramK = (FdotC <= 0.0f ? 1.0f : 0.0f);
             float excludeDepth = (excludeFarPixels ? 1.0f : 2.0f);
-            fogMaterial.SetMatrix("_FrustumCornersWS", frustumCornersArray);
+            fogMaterial.SetMatrix("_FrustumCornersWS", frustumCorners);
             fogMaterial.SetVector("_CameraWS", camPos);
             fogMaterial.SetVector("_HeightParams", new Vector4(height, FdotC, paramK, heightDensity * 0.5f));
             fogMaterial.SetVector("_DistanceParams", new Vector4(-Mathf.Max(startDistance, 0.0f), excludeDepth, 0, 0));
@@ -94,7 +116,36 @@ namespace UnityStandardAssets.ImageEffects
                 pass = 1; // distance only
             else
                 pass = 2; // height only
-            Graphics.Blit(source, destination, fogMaterial, pass);
+            CustomGraphicsBlit(source, destination, fogMaterial, pass);
+        }
+
+        static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int passNr)
+        {
+            RenderTexture.active = dest;
+
+            fxMaterial.SetTexture("_MainTex", source);
+
+            GL.PushMatrix();
+            GL.LoadOrtho();
+
+            fxMaterial.SetPass(passNr);
+
+            GL.Begin(GL.QUADS);
+
+            GL.MultiTexCoord2(0, 0.0f, 0.0f);
+            GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+            GL.MultiTexCoord2(0, 1.0f, 0.0f);
+            GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+            GL.MultiTexCoord2(0, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+            GL.MultiTexCoord2(0, 0.0f, 1.0f);
+            GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+
+            GL.End();
+            GL.PopMatrix();
         }
     }
 }
