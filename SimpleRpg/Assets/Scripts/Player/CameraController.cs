38e52ec776a5cd6c897384489cca4a579c6f1c13
Erik Vestesen <Craster013014@live.dk>
2016-12-20 03:50:47 +0100

    Multiplayer --Started

---
+++ SimpleRpg/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 myPos;
+    public Transform myPlay;
+
+    public void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+}
---
+++ SimpleRpg/Assets/Scripts/World/WorldInteractions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class WorldInteractions : NetworkBehaviour {
+    NavMeshAgent playerAgent;
+
+	// Use this for initialization
+	void Start () {
+        playerAgent = GetComponent<NavMeshAgent>();
+	}
+	    
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        // GetMouseButtonDown(0) == venstre klik
+        // !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject) Holder musen over f.eks. inventory ting
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+            GetInteraction();
+        }	
+	}
+
+    void GetInteraction() {
+        //laver en Ray fra camera ud fra musens position
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if (Physics.Raycast(interactionRay,out interactionInfo, Mathf.Infinity)) {
+            GameObject interactedObject = interactionInfo.collider.gameObject; //Det objekt interactionRay rammer, hvis den rammer noget.
+            if (interactedObject.tag == "Interactable Object") {
+                interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
+            } else {
+                //pathfinding
+                playerAgent.stoppingDistance = 0f;
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+}
