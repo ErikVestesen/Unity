16389fac757e600def6fb48b9e50edb0ea3e1c56
Erik Vestesen <Craster013014@live.dk>
2017-01-21 01:24:38 +0100

    Something

---
+++ SimpleRpg/Assets/Scripts/Items/Axe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour, IWeapon {
+    public List<BaseStat> Stats { get; set; }
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PerformAttack()
+    {
+        animator.SetTrigger("Base_Attack");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<IEnemy>().TakeDamage(Stats[0].GetCalculatedStatValue());
+        }
+        Debug.Log("Hit: " + other.name);
+    }
+
+}
--- SimpleRpg/Assets/Scripts/Player/InventoryController.cs
+++ SimpleRpg/Assets/Scripts/Player/InventoryController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour {
     public Item sword;
+    public Item axe;
+
     public PlayerWeaponController playerWeaponController;
 
 
     void Start()
     {
         playerWeaponController = GetComponent<PlayerWeaponController>();
+
         List<BaseStat> swordStats = new List<BaseStat>();
         swordStats.Add(new BaseStat(6, "Power", "Your Power level"));
         sword = new Item(swordStats, "Sword");
+
+        List<BaseStat> axeStats = new List<BaseStat>();
+        axeStats.Add(new BaseStat(2, "Power", "Power level"));
+        axe = new Item(axeStats, "Axe");
     }
 
     void Update()
@@ -20,6 +27,8 @@ public class InventoryController : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.V))
         {
             playerWeaponController.EquipWeapon(sword);
+        } else if(Input.GetKeyDown(KeyCode.B)) {
+            playerWeaponController.EquipWeapon(axe);
         }
     }
 }
